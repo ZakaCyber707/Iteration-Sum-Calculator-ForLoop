a8acaac448f05a2d9133817b6ae688b6e7554579
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-02-25 21:23:50 +0500

    Setup iterative (loop) sum accumulation logic
    "Initial setup of a program that calculates the sum of a series. It takes a number from the user and uses a for loop to add every number from 1 to that limit together, printing the final total at the end."

---
+++ sum in for loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int sum=0;
+	int num=0;
+	
+	cout<<"enter number";
+	cin>>num;
+for(int i=1;i<=num;i++)
+	{
+	cout<<i<<endl;
+	sum=sum+i;
+		
+	}
+	cout<<sum;
+	
+}
